d10190df8ccbd25bac324d49959757b6ca6d09e9
mnemonyc <mnemonyc@hotmail.com>
2017-02-03 13:04:19 +0100

    z2_plus: Remove dexpreopt for all build type for the moment

--- device.mk
+++ device.mk
@@ -95,7 +95,7 @@ TARGET_SCREEN_WIDTH := 1080
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # Dalvik overrides
-+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
 
 # Haters gonna hate..
 PRODUCT_CHARACTERISTICS := nosdcard
