47478f269bcdcf82b08d35c5906f1eec602a8f81
mnemonyc <mnemonyc@hotmail.com>
2017-02-08 21:36:16 +0100

    Revert "z2_plus: HWUI and Dalvik overrides"
    This reverts commit 76b5b6690aa664b74059cbf5bb6ccbc665d80eb8.

--- system_prop.mk
+++ system_prop.mk
@@ -85,6 +85,30 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.loc.nlp_name=com.qualcomm.location \
     ro.gps.agps_provider=1
 
+# Dalvik properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m \
+    dalvik.vm.dex2oat-min-free-mem=209715200
+
+# HWUI properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024
+
 # Hdmi
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.speaker.prot.enable=true \
