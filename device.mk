5cd6122c699596c62f7677b8b2ba7b995ac1767b
SaladaSs64 <parshad4@gmail.com>
2017-02-20 21:02:13 +0400

    add miracast props

--- device.mk
+++ device.mk
@@ -294,3 +294,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat
 
+#miracast props
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.wfd.enable=1
