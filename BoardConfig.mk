5ba46bca1687ba6f9fb359f6fc12b66124689757
mnemonyc <mnemonyc@hotmail.com>
2017-03-18 16:49:57 +0100

    z2_plus: update README. Update BoardConfig to use SnapdragonCamera

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2013-2016, The CyanogenMod Project
-# Copyright (C) 2017, The LineageOS Project
+# Copyright (C) 2017, The Discovery ROM Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -112,6 +112,7 @@ QCOM_BT_USE_SMD_TTY := true
 # Camera
 BOARD_QTI_CAMERA_32BIT_ONLY := true
 USE_DEVICE_SPECIFIC_CAMERA := true
+TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
