ba4a19c2dd25a7d10356d2a9543a03ce44721396
dianlujitao <dianlujitao@lineageos.org>
2017-08-21 22:27:14 +0800

    z2_plus: Add VR-ready power HAL
    Change-Id: I77a30defb965efba75f56bee65e9a58d30f51450

--- power/Android.mk
+++ power/Android.mk
@@ -5,26 +5,33 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
-LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2
+LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c
+LOCAL_C_INCLUDES := external/libxml2/include \
+                    external/icu/icu4c/source/common
 
-# Include target-specific files.
 ifeq ($(call is-board-platform-in-list, msm8996), true)
 LOCAL_SRC_FILES += power-8996.c
+LOCAL_CFLAGS += -DMPCTLV3
 endif
 
-ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
-    LOCAL_CFLAGS += -DINTERACTION_BOOST
+ifneq ($(TARGET_POWERHAL_SET_INTERACTIVE_EXT),)
+LOCAL_CFLAGS += -DSET_INTERACTIVE_EXT
+LOCAL_SRC_FILES += ../../../../$(TARGET_POWERHAL_SET_INTERACTIVE_EXT)
 endif
-
-#ifneq ($(TARGET_USES_AOSP),true)
-#    LOCAL_CFLAGS += -DEXTRA_POWERHAL_HINTS
-#endif
 
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
-LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+  LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+endif
+
+ifeq ($(TARGET_POWER_SET_FEATURE_LIB),)
+  LOCAL_SRC_FILES += power-feature-default.c
+else
+  LOCAL_STATIC_LIBRARIES += $(TARGET_POWER_SET_FEATURE_LIB)
 endif
 
 LOCAL_MODULE := power.msm8996
 LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
 include $(BUILD_SHARED_LIBRARY)
