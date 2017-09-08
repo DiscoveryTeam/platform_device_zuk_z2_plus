de5572eb92ec09aff4d5418749ee969392fd946c
Luca Stefani <luca020400@lineageos.org>
2017-09-08 22:32:21 +0200

    wifi: Build wifilogd

--- device.mk
+++ device.mk
@@ -275,9 +275,8 @@ PRODUCT_PACKAGES += \
     ipacm \
     ipacm-diag \
     IPACM_cfg.xml \
-    libqsap_sdk \
-    libQWiFiSoftApCfg \
     libwpa_client \
+    wifilogd \
     hostapd \
     readmac \
     dhcpcd.conf \
