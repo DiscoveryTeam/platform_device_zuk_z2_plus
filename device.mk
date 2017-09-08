b90122c6ee0c49adfb5f15e5376ba7380e486af2
Luca Stefani <luca020400@lineageos.org>
2017-09-08 22:39:26 +0200

    Build USB HAL

--- device.mk
+++ device.mk
@@ -275,6 +275,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     sensors.msm8996
 
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
