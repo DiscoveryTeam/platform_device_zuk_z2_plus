bce67780eb145eca987f6380f5a673baed6bc321
mnemonyc <mnemonyc@hotmail.com>
2017-09-15 12:17:57 +0200

    z2_plus: add hal1 camera hal

--- device.mk
+++ device.mk
@@ -122,8 +122,10 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     Camera2 \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service
 
 # Display
 PRODUCT_PACKAGES += \
