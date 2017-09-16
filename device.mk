b712e0d6716c6a32deae1de024224730a051f9d2
mnemonyc <mnemonyc@hotmail.com>
2017-09-16 07:52:24 +0200

    z2_plus: back to display CAF HALs

--- device.mk
+++ device.mk
@@ -134,9 +134,13 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    copybit.msm8996 \
     gralloc.msm8996 \
     hwcomposer.msm8996 \
-    memtrack.msm8996
+    memtrack.msm8996 \
+    libgenlock \
+    liboverlay \
+    libtinyxml
 
 # DRM
 PRODUCT_PACKAGES += \
