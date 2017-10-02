f639fd180cb13de826dca3c9d189f41ca5b9b8f6
davidevinavil <davidevinavil@gmail.com>
2017-10-02 17:21:49 +0200

    camera: Move to android.hardware.camera.provider@2.4-impl.legacy

--- device.mk
+++ device.mk
@@ -125,7 +125,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Camera2 \
     camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl
+    android.hardware.camera.provider@2.4-impl.legacy
 
 # Display
 PRODUCT_PACKAGES += \
