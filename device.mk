93fdeb8cd32fd581155cde4a2c841b472ee616e3
Demon000 <demonsingur@gmail.com>
2017-10-06 01:24:11 +0200

    camera: Add camera wrapper

--- device.mk
+++ device.mk
@@ -121,7 +121,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Camera2 \
     camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl
+    android.hardware.camera.provider@2.4-impl \
+    camera.msm8996
 
 # Display
 PRODUCT_PACKAGES += \
