ddd46e5c3c58d544064ece3be2011bf4eac49c74
davide <davidevinavil@gmail.com>
2017-12-16 21:21:08 +0100

    gps: Move libloc_api_v02 to vendor and update blobs

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -113,7 +113,7 @@ ifeq (\$(TARGET_DEVICE),$DEVICE)
 include \$(CLEAR_VARS)
 LOCAL_MODULE := libloc_api_v02
 LOCAL_MODULE_OWNER := zuk
-LOCAL_SRC_FILES := proprietary/lib64/libloc_api_v02.so
+LOCAL_SRC_FILES := proprietary/vendor/lib64/libloc_api_v02.so
 LOCAL_MULTILIB := 64
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
