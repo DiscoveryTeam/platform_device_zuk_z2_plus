cd2d829b661b1b49bb04c61bf3fbb131d46ff555
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-17 00:01:03 +0100

    Remove deprecated colorservice APK and update [2/2]

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -68,7 +68,6 @@ PRODUCT_PACKAGES += \\
     TimeService \\
     CNEService \\
     qcrilmsgtunnel \\
-    colorservice \\
     qcrilhook \\
     libqti_performance \\
     ims \\
@@ -198,18 +197,6 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
 LOCAL_MODULE_SUFFIX := .apk
 LOCAL_PRIVILEGED_MODULE := true
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := colorservice
-LOCAL_MODULE_OWNER := zuk
-LOCAL_SRC_FILES := proprietary/vendor/app/colorservice/colorservice.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_PROPRIETARY_MODULE := true
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
