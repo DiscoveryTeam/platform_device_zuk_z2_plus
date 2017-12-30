491f7cccb0d1f6cdf2580bba6dd0b7083420642a
davide <davidevinavil@gmail.com>
2017-12-30 19:49:47 +0100

    camera: add camera.device@3.2-impl

--- device.mk
+++ device.mk
@@ -121,6 +121,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl \
     camera.msm8996 \
     Camera2 \
     libshim_camera
